79b6c2bed17bfb413d3d536a702ac550b5e3fc72
olga_demina <olga.demina1410@gmail.com>
2024-03-27 13:03:02 +0100

    fix: folders name

--- intro/js/script.js
+++ intro/js/script.js
@@ -1,43 +1,80 @@
-const rowElem = document.querySelector(".row"); // object | null
-console.log(rowElem);
+// 1. Scrivo i numeri da 1 a 10
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// // ...
+// console.log(10);
 
-// for (let i = 0; i <= 1000; i++) {
+// Parti da 1
+// stampa il numero
+// incremeta il numero
+// Se il numero è minore o uguale a 10
+// riparti dall'inizios
+
+// Scrivizmolo con il ciclo for
+// for (let i = 1; i <= 10; i++) {
 //   console.log(i);
-//   let bgClass; 
-//   if( i % 2 === 0) {
-//     console.log("pari");
-//     bgClass = "even";
-//   } else {
-//     console.log("dispari");
-//     bgClass = "odd";
-//   }
-// 
-//   const boxElem = `<div class="box ${bgClass}">${i}</div>`; // string
-//   rowElem.innerHTML += boxElem;
 // }
- 
-// Creiamo i box direttamente conìme elemnti HTML
 
-for (let i = 0; i <= 10; i++) {
-  console.log(i);
-  let bgClass; 
-  if( i % 2 === 0) {
-    console.log("pari");
-    bgClass = "even";
-  } else {
-    console.log("dispari");
-    bgClass = "odd";
-  }
+// Operatore di incremento
+// let i = 0;
+// i = i + 1; ---> i++;
+// let i = 1;
+// console.log(i++); // --> 1
+// console.log(i); // --> 2
 
-  const boxElem = document.createElement("div"); // object
-  boxElem.innerHTML = i;
-  boxElem.classList.add("box");
-  boxElem.classList.add(bgClass);
-  console.log(boxElem);
+// console.log(++i); // --> 2
+// console.log(i); // --->2
 
-  boxElem.addEventListener("click", function() {
-    alert(`Ciao sono numero ${i} e sono ${bgClass}`);
-  });
-  // per inserire l'elemento nell'elemento genitore in pagina 
-  rowElem.append(boxElem);
+// let a = 10;
+// let b = 5;
+// const c = b + a++;
+// console.log(c); // ---> 15
+// console.log(a); // --> 11
+// ++i;
+
+// Chiedere all'utente di inserire un numero fino a quale vuole contare
+// const userNumber = parseInt(prompt("Fino a che numero vuoi contare?")); // number | NaN
+//
+// for(let i = 1; i <= userNumber; i++) {
+//   // scope di blocco
+//   console.log(i);
+// }
+
+//
+// for(let i = 0; i < 15; i++) {
+//   console.log(i);
+// }
+
+// Ciclando sui numeri da 1 a 15 stampare solo quelli pari
+// for (let i = 1; i <= 15; i++) {
+//   if (i % 2 !== 0) {
+//     console.log(i);
+//   }
+// }
+
+// Stampare numeri da 10 a 0 
+// for (let i = 10; i >= 0; i--) {
+//   console.log(i);
+// }
+
+// Calcolare la somma di tutti i numeri da 1 a 10
+// let sum = 0; // 10 
+// for (let i = 1; i <= 4; i++) {
+//   console.log(i); // 
+//   sum = sum + i; // 
+//   console.log('somma', sum);
+// }
+
+// Stampare 100 numeri negli li nella lista
+const resultListElem = document.getElementById("result-list"); // object | null
+console.log(resultListElem);
+for(let i = 1; i <= 100; i++) {
+  // Test
+  // console.log(i);
+  // creo li con il numero
+  const itemElem = `<li>numero: ${i}</li>`;
+  console.log(itemElem);
+  // resultListElem.innerHTML = resultListElem.innerHTML + itemElem;
+  resultListElem.innerHTML += itemElem;
 }
